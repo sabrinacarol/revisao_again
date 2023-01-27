319aaa58f680efce2c08bb98558a4b0271bcfd17
Sabrina Carol <sabrinacarol.9@gmail.com>
2023-01-26 21:34:57 -0300

    Novos arquivos .py

--- aula19.py
+++ aula19.py
@@ -8,12 +8,8 @@ number = input('Vou dobrar o número que você digitar: ')
 number_float = float(number)
 print(f'O dobro de {number} é {number_float * 2:.0f}')
 
-# Sempre que o usuário mandar um valor 
+# Sempre que o usuário mandar um valor
 # você deve tratar esse valor
 
 number_str = input('Digite um valor: ')
 print(number_str.isdigit())
-
-
-
-
