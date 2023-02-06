f2b73fa32b6f9ed0007d5857a7a5b2bc11b41238
Sabrina Carol <sabrinacarol.9@gmail.com>
2023-02-06 08:14:26 -0300

    Arquivo .py alterado

--- aula23.py
+++ aula23.py
@@ -3,12 +3,20 @@ Repetições
 while (enquanto)
 executa uma ação enquanto uma condição for verdadeira
 '''
-condição = True
+# condição = True
 
-while condição:
-    nome = input('qual é seu nome? ')
-    print(f'seu nome é {nome}')
+# while condição:
+#     nome = input('qual é seu nome? ')
+#     print(f'seu nome é {nome}')
 
-    if nome == 'sair':
-        break
-print('Acabou!')
+#     if nome == 'sair':
+#         break
+# print('Sair')
+
+contador = 0
+
+while contador <= 15:
+    contador = contador + 1
+    print(contador)
+
+print('Acabou')
