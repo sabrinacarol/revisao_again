dfa79c79d07e0999ae0c023c40ca948a6457d5ef
Sabrina Carol <sabrinacarol.9@gmail.com>
2023-02-06 07:56:56 -0300

    Novo arquivo .py criado

--- aula23.py
+++ aula23.py
@@ -1,4 +1,14 @@
 '''
 Repetições
 while (enquanto)
-executa uma'''
+executa uma ação enquanto uma condição for verdadeira
+'''
+condição = True
+
+while condição:
+    nome = input('qual é seu nome? ')
+    print(f'seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+print('Acabou!')
