c673a2e009c179c3d6c871cb17f75cacd98b6de1
Lazaro <joselazaroneto97@gmail.com>
2023-02-25 09:55:42 -0300

    Novos arquivos .py adicionados

--- aula36.py
+++ aula36.py
@@ -5,14 +5,18 @@ join - une uma string
 '''
 
 frase = 'Olha só que, coisa interessante'
-lista_frases = frase.split(',')
+lista_frases_cruas = frase.split(',')
 
-for i, frase in enumerate(lista_frases):
-    print(lista_frases[i].rstrip())
+lista_frases = []
+for i, frase in enumerate(lista_frases_cruas):
+    lista_frases.append(lista_frases_cruas[i].strip())
 
-print(lista_frases)
+# print(lista_frases_cruas)
+# print(lista_frases)
 
 
+frases_unidas = ', '.join(lista_frases)
+print(frases_unidas)
 # exemplo
 # myTuple = ("John", "Peter", "Vicky")
 
---
+++ aula37.py
@@ -0,0 +1,32 @@
+''' 
+Lista de listas e seus índices
+'''
+
+salas = [
+    # 0 nº indice 1
+    ['João', 'Sabrina', ],  # 0 nº da lista
+    # 0
+    ['Maria', ],  # 1 nº da lista
+    # 0           1        2
+    ['Lazaro', 'Pedro', 'Miguel', (0, 10, 20, 30, 40)],  # 2 nº da lista
+]
+
+# print(salas[1][0])
+# print(salas[0][1])
+# print(salas[2][3])
+# print(salas[2][3][2])
+# Entra na (lista 2) e depois entra na (tupla de indice 3) e depois mostra o (indice 3)
+
+# Na primeira [lista] na segunda o [índice]
+
+# for sala in salas:
+#    print(sala)
+
+for sala in salas:
+    print(f'A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
+
+# para cada sala nas salas me retorne a sala
+# para cada aluno na sala me retorne aluno
+# entao print aluno
