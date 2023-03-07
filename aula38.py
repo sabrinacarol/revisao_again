89f55880f338f503009e40c25aee53ac3f0d0d65
Sabrina Carol <sabrinacarol.9@gmail.com>
2023-03-07 10:59:59 -0300

    Novo arquivo .py criado

---
+++ aula38.py
@@ -0,0 +1,10 @@
+'''
+Operação ternária (condicional de uma linha)
+<valor> if <condicao> else <outro valor>
+'''
+
+# condicao = 10 == 10
+# variavel = 'Valor' if condicao else 'Outro valor'
+# print(variavel)
+
+digito = 9
