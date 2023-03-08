dfd443481ee7c2ff9281e6de2ebc285c2bc265fd
SabrinaCarol <92038640+sabrinacarol@users.noreply.github.com>
2023-03-08 11:09:14 -0300

    Update aula38.py
    Alterando o arquivo .py

--- aula38.py
+++ aula38.py
@@ -8,3 +8,6 @@ Operação ternária (condicional de uma linha)
 # print(variavel)
 
 digito = 9
+# novo_digito = digito if digito <= 9 else 0
+novo_digito = 0 if digito > 9 else digito
+print(novo_digito)
