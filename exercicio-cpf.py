62f42eabd12879abfcba846c0de946ee5970d7d9
Sabrina Carol <sabrinacarol.9@gmail.com>
2023-03-29 13:50:25 -0300

    Adicionado arquivo .py

---
+++ exercicio-cpf.py
@@ -0,0 +1,16 @@
+'''
+CPF: 605.678.813-00
+'''
+
+soma = 60+0+40+42+42+40+32+3+6
+print(soma)
+
+multiplicar = soma * 10
+print(multiplicar)
+
+resto_divisao = multiplicar % 11
+print(resto_divisao)
+
+cpf ='60567881300'
+nove_digitos = cpf[:9]
+print(nove_digitos)
