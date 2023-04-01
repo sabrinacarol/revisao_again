250de54130ab0faa8369b1cad1d06fb034f01ffb
Sabrina Carol <sabrinacarol.9@gmail.com>
2023-04-01 16:23:07 -0300

    Novo arquivo .py adicionado

--- exercicio-cpf.py
+++ exercicio-cpf.py
@@ -10,43 +10,37 @@ CPF: 605.678.813-00
 
 # resto_divisao = multiplicar % 11
 # print(resto_divisao)
-import re
 
-# cpf_enviado_usuario = re.sub('60567881300') \
-#     .replace('.', '') \
-#     .replace(' ', '') \
-#     .replace('-', '')
-cpf_enviado_usuario = re.sub(
-    r'[^0-9]',
-    '',
-    '60567881300'
-)
-print(cpf_enviado_usuario)
-nove_digitos = cpf_enviado_usuario[:9]
-contador_regressivo_1 = 10
+import random
+import sys
 
-resultado_digito_1 = 0
-for digito in nove_digitos:
-    resultado_digito_1 += int(digito) * contador_regressivo_1
-    contador_regressivo_1 -= 1
-digito_1 = (resultado_digito_1 * 10) % 11
-digito_1 = digito_1 if digito_1 <= 9 else 0
+for _ in range(100):
+    nove_digitos = ''
+    for i in range(9):
+        nove_digitos += str(random.randint(0, 9))
 
-dez_digitos = nove_digitos + str(digito_1)
-contador_regressivo_2 = 10
+    contador_regressivo_1 = 10
 
-# só para vizualizar o que tem (print(dez_digitos))
+    resultado_digito_1 = 0
+    for digito in nove_digitos:
+        resultado_digito_1 += int(digito) * contador_regressivo_1
+        contador_regressivo_1 -= 1
+    digito_1 = (resultado_digito_1 * 10) % 11
+    digito_1 = digito_1 if digito_1 <= 9 else 0
 
-resultado_digito_2 = 0
-for digito in dez_digitos:
-    resultado_digito_2 += int(digito) * contador_regressivo_2
-    contador_regressivo_2 -= 1
-digito_2 = (resultado_digito_2 * 10) % 11
-digito_2 = digito_2 if digito_2 <= 9 else 0
+    dez_digitos = nove_digitos + str(digito_1)
+    contador_regressivo_2 = 11
 
-cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+    # só para vizualizar o que tem
+    # (print(dez_digitos))
 
-if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
-    print(f'{cpf_enviado_usuario} é válido')
-else:
-    print('CPF INVÁLIDO')
+    resultado_digito_2 = 0
+    for digito in dez_digitos:
+        resultado_digito_2 += int(digito) * contador_regressivo_2
+        contador_regressivo_2 -= 1
+    digito_2 = (resultado_digito_2 * 10) % 11
+    digito_2 = digito_2 if digito_2 <= 9 else 0
+
+    cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+
+    print(cpf_gerado_pelo_calculo)
