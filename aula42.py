488df10196369ac8513e7b54a033f2d6938a3998
Sabrina Carol <sabrinacarol.9@gmail.com>
2023-04-12 10:23:24 -0300

    New file .py

---
+++ aula42.py
@@ -0,0 +1,11 @@
+'''
+Valores padrão para parâmetros 
+ao definir uma função, os parâmetros podem
+ter valores padrão. Caso o valor não seja
+enviado para o parâmetro, o valor padrão será usado.
+Refatorar: É editar o seu código.
+
+'''
+
+def soma(x, y, z=0):
+    print(x + y + z)
