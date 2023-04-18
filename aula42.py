e1e8b82d3466dbdec0f38d0d5c13ba9388bf1182
Sabrina Carol <sabrinacarol.9@gmail.com>
2023-04-18 00:01:46 -0300

    Modified file

--- aula42.py
+++ aula42.py
@@ -7,6 +7,8 @@ Refatorar: É editar o seu código.
 
 '''
 # Mostrar que z é none (Definir como um não valor)
+# todos os parametros que vem apos um
+#  valor nomeados teram que vir nomeados tbm.
 
 def soma(x, y, z=None):
     if z is not None:
@@ -16,3 +18,5 @@ def soma(x, y, z=None):
 
 soma(1, 2)
 soma(3, 5)
+soma(7, 9, 0)
+soma(y=9, z= 0, x=7)
