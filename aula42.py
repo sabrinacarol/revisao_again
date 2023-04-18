49bad0de18178bbbabaf49bf6c29f7acce738627
Sabrina Carol <sabrinacarol.9@gmail.com>
2023-04-17 22:45:30 -0300

    Modified file .py

--- aula42.py
+++ aula42.py
@@ -6,6 +6,13 @@ enviado para o parâmetro, o valor padrão será usado.
 Refatorar: É editar o seu código.
 
 '''
+# Mostrar que z é none (Definir como um não valor)
 
-def soma(x, y, z=0):
-    print(x + y + z)
+def soma(x, y, z=None):
+    if z is not None:
+        print(f'{x=} {y=} {z=}', x + y + z)
+    else:
+        print(f'{x=} {y=}', x + y)
+
+soma(1, 2)
+soma(3, 5)
