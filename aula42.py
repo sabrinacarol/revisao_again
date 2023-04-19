e37aec49d5b79a4f67f6673307e5d3d7a1a19b15
Sabrina Carol <sabrinacarol.9@gmail.com>
2023-04-19 10:36:57 -0300

    File modified .py

--- aula42.py
+++ aula42.py
@@ -5,10 +5,12 @@ ter valores padrão. Caso o valor não seja
 enviado para o parâmetro, o valor padrão será usado.
 Refatorar: É editar o seu código.
 
+
 '''
 # Mostrar que z é none (Definir como um não valor)
 # todos os parametros que vem apos um
 #  valor nomeados teram que vir nomeados tbm.
+
 
 def soma(x, y, z=None):
     if z is not None:
@@ -16,7 +18,8 @@ def soma(x, y, z=None):
     else:
         print(f'{x=} {y=}', x + y)
 
+
 soma(1, 2)
 soma(3, 5)
 soma(7, 9, 0)
-soma(y=9, z= 0, x=7)
+soma(y=9, z=0, x=7)
